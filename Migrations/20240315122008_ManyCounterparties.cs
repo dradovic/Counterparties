551630fd010f90ccce6163e03496adf883b46a9b
Dejan Appenzeller <dejan.appenzeller@swisspeers.ch>
2024-03-15 13:21:16 +0100

    added mig

---
+++ Migrations/20240315122008_ManyCounterparties.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Counterparties.Migrations
+{
+    /// <inheritdoc />
+    public partial class ManyCounterparties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CreditCounterparty");
+
+            migrationBuilder.CreateTable(
+                name: "Counterparties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Discriminator = table.Column<string>(type: "nvarchar(21)", maxLength: 21, nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Counterparties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CreditContractCounterparties",
+                columns: table => new
+                {
+                    ContractId = table.Column<int>(type: "int", nullable: false),
+                    CounterpartyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CreditContractCounterparties", x => new { x.ContractId, x.CounterpartyId });
+                    table.ForeignKey(
+                        name: "FK_CreditContractCounterparties_Contracts_ContractId",
+                        column: x => x.ContractId,
+                        principalTable: "Contracts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CreditContractCounterparties_Counterparties_CounterpartyId",
+                        column: x => x.CounterpartyId,
+                        principalTable: "Counterparties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CreditContractCounterparties_CounterpartyId",
+                table: "CreditContractCounterparties",
+                column: "CounterpartyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CreditContractCounterparties");
+
+            migrationBuilder.DropTable(
+                name: "Counterparties");
+
+            migrationBuilder.CreateTable(
+                name: "CreditCounterparty",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreditContractId = table.Column<int>(type: "int", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(34)", maxLength: 34, nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CreditCounterparty", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CreditCounterparty_Contracts_CreditContractId",
+                        column: x => x.CreditContractId,
+                        principalTable: "Contracts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CreditCounterparty_CreditContractId",
+                table: "CreditCounterparty",
+                column: "CreditContractId");
+        }
+    }
+}
