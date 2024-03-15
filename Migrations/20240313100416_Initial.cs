537be0dde250fbe047bc5c77486affedaeba45d9
Dejan Appenzeller <dejan.appenzeller@swisspeers.ch>
2024-03-15 12:09:12 +0100

    copied from PolymorphicCollections

---
+++ Migrations/20240313100416_Initial.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PolymorphicCollection.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contracts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contracts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CreditCounterparty",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreditContractId = table.Column<int>(type: "int", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(34)", maxLength: 34, nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CreditCounterparty", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CreditCounterparty_Contracts_CreditContractId",
+                        column: x => x.CreditContractId,
+                        principalTable: "Contracts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CreditCounterparty_CreditContractId",
+                table: "CreditCounterparty",
+                column: "CreditContractId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CreditCounterparty");
+
+            migrationBuilder.DropTable(
+                name: "Contracts");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+using (var context = new AppDbContext())
+{
+    context.Database.Migrate(); // apply any outstanding migrations
+
+    context.Contracts.Add(new CreditContract
+    {
+        Counterparties = [
+            new CorporateCreditCounterparty { CompanyName = "Lego" },
+            new PrivateCreditCounterparty { PersonName = "Charles" }
+        ],
+    });
+    context.SaveChanges();
+    Console.WriteLine("Stored.");
+}
+
+using (var context = new AppDbContext())
+{
+    foreach (var contract in context.Contracts.Include(c => c.Counterparties))
+    {
+        Console.WriteLine(contract.ToString());
+    }
+}
+
+public class CreditContract
+{
+    public int Id { get; set; }
+
+    public ICollection<CreditCounterparty> Counterparties { get; set; } = null!;
+
+    public override string ToString()
+    {
+        return string.Join(", ", Counterparties.Select(c => c.ToString()));
+    }
+}
+
+public abstract class CreditCounterparty
+{
+    public int Id { get; set; }
+    public int CreditContractId { get; set; }
+}
+
+public class PrivateCreditCounterparty : CreditCounterparty
+{
+    public string? PersonName { get; set; }
+
+    public override string ToString()
+    {
+        return $"Private: {PersonName}";
+    }
+}
+
+public class CorporateCreditCounterparty : CreditCounterparty
+{
+    public string? CompanyName { get; set; }
+
+    public override string ToString()
+    {
+        return $"Corp: {CompanyName}";
+    }
+}
+
+public class AppDbContext : DbContext
+{
+    public DbSet<CreditContract> Contracts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<CorporateCreditCounterparty>()
+            .HasBaseType<CreditCounterparty>();
+        builder.Entity<PrivateCreditCounterparty>()
+            .HasBaseType<CreditCounterparty>();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=PolymorphicCollection;Trusted_Connection=True;MultipleActiveResultSets=True;Encrypt=False");
+    }
+}
